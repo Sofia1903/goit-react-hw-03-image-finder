39dfb9323b9fab8f273dc59efc7120a487516e00
Sofia1903 <s.skriabina19@gmail.com>
2023-06-01 13:55:29 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,114 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import { Global } from '@emotion/react';
+import { Notify } from 'notiflix';
+import Searchbar from "./Searchbar";
+import { Spinner } from "./Spinner/Spinner";
+import { fetchImage } from "service/api-pixabay";
+import { ImageGallery } from "./ImageGallery/ImageGallery";
+import { StyledContainer, StyledButtonLoad  } from "./App.styled";
+import '../styles/spin.css'
+import { global } from "styles/global-styles";
+import { startSpinner, stopSpinner } from "service/api-spinner";
+
+export class App extends Component {
+  state = {
+    inputValue: '',
+    pageNumber: '',
+    images: '',
+    isButtonDisabled: false,
+  };
+
+  componentDidUpdate(_, prevState){
+    if(prevState.inputValue !== this.state.inputValue){
+      startSpinner();
+
+      fetchImage(this.state.inputValue, this.state.pageNumber)
+      .then((images)=>{
+
+        this.setState({
+          images: images.data.hits,
+          isButtonDisabled: true,
+          pageNumber: this.state.pageNumber + 1,
+        })
+
+        if(images.data.hits.length === 0){
+          Notify.info('Пробачьте, по Вашему запиту нічого не знайдено!')
+          this.setState({
+            images: '',
+            pageNumber: 1,
+            isButtonDisabled: false,
+          })
+          stopSpinner()
+          return
+        }
+
+        if(images.data.hits.length < 12){
+          Notify.info(`по Вашему запиту знайдено ${images.data.hits.length} картинок`)
+          this.setState({
+            isButtonDisabled: false,
+          })
+        }
+
+      })
+      .catch((error)=>{
+        Notify.failure('Щось пішло не так!')
+        console.log(error)
+      })
+      .finally(()=>{
+        stopSpinner()
+      })
+    }
+  };
+
+  createRequestValue = (inputValue) => {
+    this.setState({
+      inputValue,
+      pageNumber: 1,
+    })
+  };
+
+  handlerBtnClick = () => {
+    startSpinner()
+    fetchImage(this.state.inputValue, this.state.pageNumber)
+      .then((images)=>{
+      this.setState((prevState)=>({
+        images: [...this.state.images, ...images.data.hits],
+        pageNumber: prevState.pageNumber + 1,
+      }))
+      if(images.data.hits.length < 12){
+        Notify.info('Пробачьте, по Вашему запиту більше нічого не знайдено!')
+        this.setState({
+          isButtonDisabled: false
+        })
+      }
+      })
+      .catch((error)=>{
+        Notify.failure('Щось пішло не так!')
+        console.log(error)
+      })
+      .finally(()=>{
+        stopSpinner()
+      }) 
+};
+
+  render() {
+    const {images, isButtonDisabled} = this.state
+
+    return (
+      <>
+      <Global styles={global}/>
+        <StyledContainer>
+          <Searchbar createRequestValue={this.createRequestValue}/>
+          {images && <ImageGallery images={images} isOpen={false}/>}
+          {isButtonDisabled && 
+            <StyledButtonLoad 
+            type="button"
+            onClick={this.handlerBtnClick}>
+            Load more
+            </StyledButtonLoad>}
+        </StyledContainer>
+        <Spinner/>
+      </>
+    );
+  };
 };
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,60 @@
+import { Notify } from 'notiflix';
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { StyledHeader, StyledForm, StyledFormBtn, StyledFormInput } from './Searchbar.styled';
+import IconSearch from 'service/search-svg';
+
+class Searchbar extends Component {
+  state = {
+    inputValue: '',
+  };
+
+  handleInputChange = ({target}) => {
+    this.setState({
+      inputValue: target.value.toLowerCase(),
+    })
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(this.state.inputValue) {
+      this.props.createRequestValue(this.state.inputValue);
+      this.setState({
+        inputValue: '',
+      })
+    } else {
+      Notify.failure('Поіск порожній та треба ввести значення!')
+    };
+    
+  };
+
+  render() {
+    return (
+      <StyledHeader>
+        <StyledForm onSubmit={this.handleSubmit}>
+          <StyledFormBtn type="submit">
+            <IconSearch>Search</IconSearch>
+          </StyledFormBtn>
+
+          <StyledFormInput
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={this.handleInputChange}
+            value={this.state.inputValue}
+          />
+        </StyledForm>
+      </StyledHeader>
+    )
+
+  }
+};
+
+Searchbar.propTypes = {
+  createRequestValue: PropTypes.func.isRequired
+}
+
+export default Searchbar;
+
